c1e2382cbdcabfeaa9f91d11f192b9662e3f780e
webarata3 <webarata3@gmail.com>
2018-04-26 20:11:38 +0900

    Databasae01

--- Database01/app/src/main/java/com/example/database01/DatabaseHelper.java
+++ Database01/app/src/main/java/com/example/database01/DatabaseHelper.java
@@ -5,9 +5,13 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "book";
+    private static final String DATABASE_NAME = "book.db";
 
     private static final int DATABASE_VERSION = 1;
+
+    private static final String[] INIT_DATA = {
+            "INSERT INTO book(name, price) VALUES('Android入門', 2980)",
+            "INSERT INTO book(name, price) VALUES('Java入門', 1980)"};
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -22,6 +26,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         ddl += ")";
 
         db.execSQL(ddl);
+
+        try {
+            db.beginTransaction();
+
+            for (String initData : INIT_DATA) {
+                db.execSQL(initData);
+            }
+
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
     }
 
     @Override
--- Database01/app/src/main/java/com/example/database01/MainActivity.java
+++ Database01/app/src/main/java/com/example/database01/MainActivity.java
@@ -2,17 +2,12 @@ package com.example.database01;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteStatement;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView allDataTextView;
-
-    private TextView nameTextView;
-    private TextView priceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,20 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
         allDataTextView = findViewById(R.id.allDataTextView);
 
-        nameTextView = findViewById(R.id.nameEditText);
-        priceTextView = findViewById(R.id.priceEditText);
-
-        findViewById(R.id.registerButton).setOnClickListener(view -> {
-            registerBook();
+        findViewById(R.id.viewButton).setOnClickListener(view-> {
+            view();
         });
-
-        readAllData();
     }
 
-    private void readAllData() {
+    private void view() {
         DatabaseHelper helper = new DatabaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
         String sql = "SELECT * FROM book";
-        SQLiteDatabase db = helper.getReadableDatabase();
         try (Cursor cursor = db.rawQuery(sql, null)) {
             String result = "";
             while (cursor.moveToNext()) {
@@ -42,24 +33,8 @@ public class MainActivity extends AppCompatActivity {
                 int price = cursor.getInt(cursor.getColumnIndex("price"));
                 result = result + name + "," + price + "\n";
             }
+
             allDataTextView.setText(result);
         }
-    }
-
-    private void registerBook() {
-        String name = nameTextView.getText().toString();
-        nameTextView.setText("");
-        int price = Integer.parseInt(priceTextView.getText().toString());
-        priceTextView.setText("");
-
-        DatabaseHelper helper = new DatabaseHelper(this);
-        SQLiteDatabase db = helper.getWritableDatabase();
-        String sql = "INSERT INTO book(name, price) VALUES(?, ?)";
-        SQLiteStatement stmt = db.compileStatement(sql);
-        stmt.bindString(1, name);
-        stmt.bindLong(2, price);
-        stmt.executeInsert();
-
-        readAllData();
     }
 }
