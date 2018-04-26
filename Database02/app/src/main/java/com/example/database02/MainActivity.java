82e90da1c1f7373f61699d6d743440a26d1ade14
webarata3 <webarata3@gmail.com>
2018-04-26 20:20:15 +0900

    Database02

---
+++ Database02/app/src/main/java/com/example/database02/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.database02;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView allDataTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        allDataTextView = findViewById(R.id.allDataTextView);
+
+        findViewById(R.id.viewButton).setOnClickListener(view-> {
+            view();
+        });
+    }
+
+    private void view() {
+        DatabaseHelper helper = new DatabaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        String sql = "SELECT * FROM book";
+        try (Cursor cursor = db.rawQuery(sql, null)) {
+            String result = "";
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(cursor.getColumnIndex("name"));
+                int price = cursor.getInt(cursor.getColumnIndex("price"));
+                result = result + name + "," + price + "\n";
+            }
+
+            allDataTextView.setText(result);
+        }
+    }
+}
