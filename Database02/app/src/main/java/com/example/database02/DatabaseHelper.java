11802df6ccd4b9fdf8526f6a00871b8ca956f91a
webarata3 <webarata3@gmail.com>
2018-04-29 17:41:50 +0900

    Database02をLFに

---
+++ Database02/app/src/main/java/com/example/database02/DatabaseHelper.java
@@ -0,0 +1,38 @@
+package com.example.database02;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "book.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String[] INIT_DATA = {
+            "INSERT INTO book(name, price) VALUES('Android入門', 2980)",
+            "INSERT INTO book(name, price) VALUES('Java入門', 1980)"};
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String ddl = "CREATE TABLE book("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "name TEXT,"
+                + "price INTEGER"
+                + ")";
+
+        db.execSQL(ddl);
+
+        for (String initData : INIT_DATA) {
+            db.execSQL(initData);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
