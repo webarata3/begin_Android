34a65038d88e8653a51a74004a39abfaf46b6ac2
webarata3 <webarata3@gmail.com>
2018-05-03 05:40:45 +0900

    Database05

--- Database05/app/src/main/java/com/example/database05/AppExecutors.java
+++ Database05/app/src/main/java/com/example/database05/AppExecutors.java
@@ -1,21 +1,40 @@
 package com.example.database05;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class AppExecutors {
 
     private final Executor diskIo;
+    private final Executor mainThread;
 
-    private AppExecutors(Executor diskIo) {
+    private AppExecutors(Executor diskIo, Executor mainThread) {
         this.diskIo = diskIo;
+        this.mainThread = mainThread;
     }
 
     public AppExecutors() {
-        this(Executors.newSingleThreadExecutor());
+        this(Executors.newSingleThreadExecutor(), new MainThreadExecutor());
     }
 
     public Executor diskIo() {
         return diskIo;
+    }
+
+    public Executor mainThread() {
+        return mainThread;
+    }
+
+    private static class MainThreadExecutor implements Executor {
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(@NonNull Runnable command) {
+            mainThreadHandler.post(command);
+        }
     }
 }
--- Database05/app/src/main/java/com/example/database05/MainActivity.java
+++ Database05/app/src/main/java/com/example/database05/MainActivity.java
@@ -8,12 +8,16 @@ import android.widget.TextView;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private AppExecutors appExecutors;
+
     private TextView allDataTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        appExecutors = new AppExecutors();
 
         allDataTextView = findViewById(R.id.allDataTextView);
 
@@ -22,27 +26,28 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public DatabaseHelper getDatabaseHelper() {
+        return DatabaseHelper.getInstance (this, appExecutors);
+    }
+
+    public AppDatabase getAppDatabase() {
+        return AppDatabase.getInstance(getDatabaseHelper());
+    }
+
     private void view() {
-        AppDatabase appDatabase = ((BasicApp) getApplication()).getAppDatabase();
+        AppDatabase appDatabase = getAppDatabase();
 
         appDatabase.executeQuery(db -> {
             BookDao bookDao = new BookDao(db);
             List<Book> bookList = bookDao.selectAll();
 
-            test(bookList);
-        });
-    }
-
-    private void test(List<Book> bookList) {
-        Handler handler = new Handler(getMainLooper());
-        new Thread(() -> {
-            handler.post(() -> {
+            appExecutors.mainThread().execute(() -> {
                 String listText = "";
                 for (Book book : bookList) {
                     listText = listText + book.getName() + "," + book.getPrice() + "\n";
                 }
                 allDataTextView.setText(listText);
             });
-        }).start();
+        });
     }
 }
