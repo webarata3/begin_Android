bd6ad7d8d96814ea9bc5e0e64f7b5e84136d21b6
webarata3 <webarata3@gmail.com>
2018-10-11 14:13:12 +0900

    AndroidXを使用

--- Database03/app/src/main/java/com/example/database03/MainActivity.java
+++ Database03/app/src/main/java/com/example/database03/MainActivity.java
@@ -4,10 +4,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TextView allDataTextView;
