4d87a12e1108073495490e2a2a8dec1417443e5e
webarata3 <webarata3@gmail.com>
2018-10-12 18:57:18 +0900

    View01～08

---
+++ View03/app/src/main/java/com/example/view03/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.view03;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "ボタンが押された", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
